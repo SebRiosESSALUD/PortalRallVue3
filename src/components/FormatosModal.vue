e9811cea839f7875324c2c751e8804e661a82efb
Sebastian Rios <sebastianjoserc.essalud@gmail.com>
2025-02-14 12:53:23 -0500

    headeral80%

---
+++ src/components/FormatosModal.vue
@@ -0,0 +1,29 @@
+<template>
+    <div v-if="show" class="modal fade show d-block" tabindex="-1">
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Formatos</h5>
+            <button type="button" class="btn-close" @click="$emit('close')"></button>
+          </div>
+          <div class="modal-body">
+            <!-- Contenido del modal -->
+            <p>Contenido de formatos...</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  defineProps({
+    show: Boolean
+  });
+  </script>
+  
+  <style>
+  .modal.show {
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+  </style>
+  
--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 import { createApp } from 'vue'
-import './style.css'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Componentes
+import PacsView from './components/PacsView.vue'
+import AnexosView from './components/AnexosView.vue'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/pacs', component: PacsView },
+    { path: '/anexos', component: AnexosView },
+    { path: '/', redirect: '/pacs' }
+  ]
+})
+
+createApp(App)
+  .use(router)
+  .mount('#app')
