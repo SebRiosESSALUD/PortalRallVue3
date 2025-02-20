6592010471d9c7ee5f1415df1df03d9376693c16
Sebastian Rios <sebastianjoserc.essalud@gmail.com>
2025-02-20 13:01:19 -0500

    reorganizacion

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <script setup>
 import HeaderRall from './components/HeaderRall.vue';
+import FooterRall from './components/FooterRall.vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
 </script>
 
 <template>
-  <HeaderRall />
-  <div class="container-fluid bg-primary p-2 text-dark bg-opacity-10" style="margin-top: 50px;">
-    <router-view></router-view>
-  </div>
+
+<header>
+  <HeaderRall/>
+</header>
+
+<body class="cuerpoPrincipal">
+  <router-view></router-view>
+</body>
+
+<footer>
+  <FooterRall/>
+</footer>
 </template>
+
+<style lang="scss">
+@import './assets/css/app.css';
+</style>
