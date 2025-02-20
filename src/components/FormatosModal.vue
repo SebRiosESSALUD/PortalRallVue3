616e2ad04344cb1b59f84b71f449c5be2ce1c981
Sebastian Rios <sebastianjoserc.essalud@gmail.com>
2025-02-20 10:10:35 -0500

    cambiomodales

--- src/components/FormatosModal.vue
+++ src/components/FormatosModal.vue
@@ -1,39 +1,45 @@
 <template>
   <div v-if="show" class="modal fade show d-block" tabindex="-1">
-    <div class="modal-dialog modal-lg">
+    <div class="modal-dialog modal-xl">
       <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title">Formatos</h5>
-        </div>
         <div class="modal-body" id="modalesPrincipales">
           <!-- Contenido del modal de transparencia -->
           <!-- Fila superior recursos humanos -->
           <div class="row" id="filaFormatos">
-            <h3 style="background-color: #3264a8; border-radius: 10px; color: white;" > <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
-              <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5"/>
-            </svg> Recursos Humano</h3>
+            <h5 style="color: #454545;"> 
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
+                <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5"/>
+              </svg> Recursos Humanos
+            </h5>
+            <hr>
             <div class="col">
-              <a href="">Formato convenios y vacaciones</a>
+              <a id="enlaceModalFormatos" href="">Formato convenios y vacaciones</a>
             </div>
             <div class="col">
-              <router-link to="/papeletas">Formato Papeletas</router-link>
+                <router-link id="enlaceModalFormatos" to="/papeletas">Formato Papeletas</router-link> 
             </div>
           </div>
 
           <!-- Fila ingerior soporte informatico -->
           <div class="row" id="filaFormatos">
-            <h3 style="background-color: #3264a8; border-radius: 10px; color: white;" >Soporte Informático - Formatos</h3>
+            <h5 style="color: #454545;">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-laptop" viewBox="0 0 16 16">
+                <path d="M13.5 3a.5.5 0 0 1 .5.5V11H2V3.5a.5.5 0 0 1 .5-.5zm-11-1A1.5 1.5 0 0 0 1 3.5V12h14V3.5A1.5 1.5 0 0 0 13.5 2zM0 12.5h16a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5"/>
+              </svg>
+              Soporte Informático - Formatos
+            </h5>
+            <hr>
             <div class="col">
-              <a href="">Formato VPN</a>
+              <a id="enlaceModalFormatos" href="">Formato VPN</a>
             </div>
             <div class="col">
-              <a href="">Formatos SIAD</a>
+              <a id="enlaceModalFormatos" href="">Formatos SIAD</a>
             </div>
             <div class="col">
-              <a href="">Formato para acceso a internet</a>
+              <a id="enlaceModalFormatos" href="">Formato para acceso a internet</a>
             </div>
             <div class="col">
-              <a href="">Capacitación</a>
+              <a id="enlaceModalFormatos" href="">Capacitación</a>
             </div>
           </div>
         </div>
@@ -88,6 +94,17 @@ onUnmounted(() => {
 
 #filaFormatos {
   margin: 20px;
+}
+
+#enlaceModalFormatos {
+  color: black;
+  text-decoration: none;
+  transition: color 0.3s ease,  0.3s ease;
+}
+
+#enlaceModalFormatos:hover {
+  color: blue;
+  font-size: 110%;
 }
 
 </style>
--- src/components/TransparenciaModal.vue
+++ src/components/TransparenciaModal.vue
@@ -1,29 +1,51 @@
+<!-- ModalBase.vue -->
 <template>
-    <div v-if="show" class="modal fade show d-block" tabindex="-1">
-      <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title">Transparencia</h5>
-            <button type="button" class="btn-close" @click="$emit('close')"></button>
-          </div>
-          <div class="modal-body">
-            <!-- Contenido del modal de transparencia -->
-            <p>Contenido de transparencia...</p>
-          </div>
+  <div v-if="show" class="modal fade show d-block" tabindex="-1">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-body">
+          <slot></slot>
         </div>
       </div>
     </div>
-  </template>
-  
-  <script setup>
-  defineProps({
-    show: Boolean
-  });
-  </script>
-  
-  <style>
-  .modal.show {
-    background-color: rgba(0, 0, 0, 0.5);
+  </div>
+</template>
+
+<script setup>
+import { onMounted, onUnmounted } from 'vue';
+
+defineProps({
+  show: Boolean,
+  title: String
+});
+
+const emit = defineEmits(['close']);
+
+const handleOutsideClick = (event) => {
+  if (event.target.classList.contains('modal')) {
+    emit('close');
   }
-  </style>
-  
+};
+
+const handleEscapeKey = (event) => {
+  if (event.key === 'Escape') {
+    emit('close');
+  }
+};
+
+onMounted(() => {
+  document.addEventListener('click', handleOutsideClick);
+  document.addEventListener('keydown', handleEscapeKey);
+});
+
+onUnmounted(() => {
+  document.removeEventListener('click', handleOutsideClick);
+  document.removeEventListener('keydown', handleEscapeKey);
+});
+</script>
+
+<style>
+.modal.show {
+  background-color: rgba(0, 0, 0, 0.5);
+}
+</style>
