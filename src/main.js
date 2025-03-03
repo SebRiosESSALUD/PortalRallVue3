765bf419180c14c036ef3ec544063cb4028316d2
darko21010 <jorge.baltodano.21.10@gmail.com>
2025-03-03 12:19:22 -0500

    Cambios anexos

--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+
 // Componentes
 import PacsView from './components/PacsView.vue'
 import AnexosView from './components/AnexosView.vue'
 import PapeletasView from './components/PapeletaRall.vue'
 import TransparenciaView from './components/TransparenciaModal.vue'
+
 
 const router = createRouter({
   history: createWebHistory(),
